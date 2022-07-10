7f96d3e61017eb0b89d6a3bcbf353739cb1ac384
IronKinoko <kinoko_main@outlook.com>
2022-07-10 21:53:11 +0800

    feat: add dev plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import inquirer from 'inquirer'
 import esbuild from 'rollup-plugin-esbuild'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import url from 'url'
 
 async function run() {
   const packages = (await glob('packages/*', { onlyDirectories: true })).filter(
@@ -25,26 +26,64 @@ async function run() {
     buildPackages = ans.package
   }
 
-  const pkgConfigs = await Promise.all(
-    buildPackages.map(async (root) => {
-      const pkg = await fs.readJson(path.resolve(root, 'package.json'))
-      const meta = await fs.readFile(
-        path.resolve(root, 'meta.template'),
-        'utf-8'
-      )
-
-      return {
-        input: path.join(root, 'src', 'index.ts'),
-        output: {
-          file: path.join('dist', `${pkg.name}.user.js`),
-          format: 'iife',
-          banner: meta.replace('#version#', pkg.version),
-        },
-        plugins: [esbuild(), commonjs(), nodeResolve({ browser: true })],
-      }
-    })
-  )
+  const pkgConfigs = await Promise.all(buildPackages.map(createConfig))
   return pkgConfigs
 }
 
 export default run()
+
+/**
+ * @param {string} root
+ * @returns {Promise<import('rollup').RollupOptions>}
+ */
+async function createConfig(root) {
+  const pkg = await fs.readJson(path.resolve(root, 'package.json'))
+  let meta = await fs.readFile(path.resolve(root, 'meta.template'), 'utf-8')
+  meta = meta.replace('#version#', pkg.version)
+
+  pkg.globals ||= {}
+
+  return {
+    input: path.join(root, 'src', 'index.ts'),
+    output: {
+      file: path.join('dist', `${pkg.name}.user.js`),
+      format: 'iife',
+      banner: meta,
+      globals: pkg.globals,
+    },
+    external: Object.keys(pkg.globals),
+    plugins: [
+      esbuild(),
+      commonjs(),
+      nodeResolve({ browser: true }),
+      argv.watch && userscriptsDev({ meta }),
+    ].filter(Boolean),
+  }
+}
+
+/**
+ * @returns {import('rollup').Plugin}
+ */
+function userscriptsDev({ meta }) {
+  return {
+    name: 'userscripts-dev',
+    outputOptions(opts) {
+      const devFile = opts.file.replace('.user.', '.dev.')
+      const metaArr = meta.trim().split('\n')
+      metaArr.splice(
+        metaArr.length - 1,
+        0,
+        `// @require      ${url.pathToFileURL(opts.file)}`
+      )
+
+      const idx = metaArr.findIndex((s) => s.includes('@name'))
+      metaArr[idx] = metaArr[idx] + ' - DEV'
+
+      const devMeta = metaArr.join('\n')
+
+      fs.ensureFile(devFile).then(() => {
+        fs.writeFile(devFile, devMeta)
+      })
+    },
+  }
+}
