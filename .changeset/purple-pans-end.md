1435bff8bd622de1d4da25d16184e171acff87ab
IronKinoko <kinoko_main@outlook.com>
2025-11-24 09:49:39 +0800

    feat: 优化 list 页面样式

---
+++ .changeset/purple-pans-end.md
@@ -0,0 +1,5 @@
+---
+'steam-multisell': patch
+---
+
+feat: 优化 list 页面样式
--- packages/steam-multisell/src/index.ts
+++ packages/steam-multisell/src/index.ts
@@ -1,6 +1,7 @@
 import { router } from 'shared'
 import * as listings from './market/listings'
 import * as multisell from './market/multisell'
+import './index.scss'
 
 router([
   { path: '/market/listings/', run: listings.run },
